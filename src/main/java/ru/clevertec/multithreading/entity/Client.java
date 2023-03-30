a78bdba72e370a85b66ee8f10ed2e836b166be59
Stanislau Kachan <Lancer397@gmail.com>
2023-03-30 20:46:55 +0300

    remove: removing import 'random' from client and server

--- src/main/java/ru/clevertec/multithreading/entity/Client.java
+++ src/main/java/ru/clevertec/multithreading/entity/Client.java
@@ -5,7 +5,6 @@ import ru.clevertec.multithreading.util.RandomNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
--- src/main/java/ru/clevertec/multithreading/entity/Server.java
+++ src/main/java/ru/clevertec/multithreading/entity/Server.java
@@ -5,7 +5,6 @@ import ru.clevertec.multithreading.util.RandomNumberGenerator;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
